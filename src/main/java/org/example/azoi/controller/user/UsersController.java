9e0cf460193522e6842fcbaf273b3f0fe1cdd01b
azathorpe <3309054750@qq.com>
2026-09-16 18:57:36 +0800

    [fix bug] simple fix

--- src/main/java/org/example/azoi/controller/user/UsersController.java
+++ src/main/java/org/example/azoi/controller/user/UsersController.java
@@ -1,11 +1,14 @@
 package org.example.azoi.controller.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 用于控制多个users相关的请求
  */
-@Controller("/users")
+@RestController
+@RequestMapping("/users")
 public class UsersController {
     //TODO: complete this part
 }
