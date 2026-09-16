84db658ac6c073518c045f3daa917152d79efc56
azathorpe <3309054750@qq.com>
2026-09-16 18:01:58 +0800

    [fix bug] FIX CrudRepository

--- src/main/java/org/example/azoi/utils/repository/TeamMemberRepository.java
+++ src/main/java/org/example/azoi/utils/repository/TeamMemberRepository.java
@@ -2,11 +2,12 @@ package org.example.azoi.utils.repository;
 
 import org.example.azoi.model.TeamMember;
 import org.example.azoi.model.TeamMemberId;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface TeamMemberRepository extends CrudRepository<TeamMember, TeamMemberId> {
+public interface TeamMemberRepository extends JpaRepository<TeamMember, TeamMemberId> {
     List<TeamMember> getAllById_TeamId(Long idTeamId);
 
     List<TeamMember> getTeamMemberById_UserId(Long idUserId);
