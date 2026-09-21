c5a9648a93f491ac7ef39dbf837e6c13e39dbbd6
Azathorpe <384735481@qq.com>
2026-09-21 18:32:00 +0800

    [FIX] 把JWT应用到了SubmitController上，并且添加了注释

--- src/main/java/org/example/azoi/controller/problem/SubmitController.java
+++ src/main/java/org/example/azoi/controller/problem/SubmitController.java
@@ -22,6 +22,13 @@ public class SubmitController {
         this.submissionService = submissionService;
     }
 
+    /**
+     * 获取所有的提交(查询)<br/>
+     * 请求地址: /submit/list<br/>
+     * 请求方法: /submit/list
+     * @param submitQueryDTO 查询(可空)
+     * @return {@link List} of {@link SubmitVO}
+     */
     @GetMapping("/list")
     public ResponseEntity<Result<List<SubmitVO>>> getSubmitList(SubmitQueryDTO submitQueryDTO) {
         // 如果没有查询条件  那就默认查询
@@ -31,6 +38,13 @@ public class SubmitController {
                 : ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result);
     }
 
+    /**
+     * 通过提交Id获取提交<br/>
+     * 请求地址: /submit/{submissionId}<br/>
+     * 请求方法: /submit/{submissionId}
+     * @param submissionId 提交Id
+     * @return {@link SubmitVO}
+     */
     @GetMapping("/{submissionId}")
     public ResponseEntity<Result<SubmitVO>> getSubmit(@PathVariable Long submissionId) {
         Result<SubmitVO> result = submissionService.getSubmit(submissionId);
